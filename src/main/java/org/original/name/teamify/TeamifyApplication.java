a925ed646cfac478192ad36d74763eca6c43ca17
Oleh Haliak <olehhaliak@gmai.com>
2023-04-29 12:42:55 +0300

    initial

---
+++ src/main/java/org/original/name/teamify/TeamifyApplication.java
@@ -0,0 +1,13 @@
+package org.original.name.teamify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/original/name/teamify/TeamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.original.name.teamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
